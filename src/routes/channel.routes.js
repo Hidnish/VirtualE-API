2406269637550f9e7113ae017086e0fd90c597bf
Hidnish <hideonishimura@live.it>
2022-09-22 10:10:42 +0200

    Add jwt middleware to addChannel route

--- src/routes/channel.routes.js
+++ src/routes/channel.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { getChannel, getChannels, addChannel, editChannelName, deleteChannel } from '../controllers/channel.controller.js';
+import { verifyTokenTest } from '../middlewares/authJwt.js'
 const router = Router();
 
 router.get('/getChannels', getChannels);
 router.get('/getChannel/:channelId', getChannel);
-router.post('/addChannel', addChannel);
+router.post('/addChannel', verifyTokenTest, addChannel); // Middleware added to verify user via jwt
 router.patch('/editChannelName/:channelId', editChannelName);
 router.delete('/deleteChannel/:channelId', deleteChannel);
 
