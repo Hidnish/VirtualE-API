528cf2cf299e4fc88e8a23a2c4e1f62b4783db5d
Hidnish <hideonishimura@live.it>
2022-09-22 10:08:47 +0200

    Add Test function to verify user auth token

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -9,4 +9,18 @@ const verifyToken = async (req, res, next) => {
     next()
 }
 
-export {verifyToken}
+// TEST function to verify token
+const verifyTokenTest = async (req, res, next) => {
+    const authHeader = req.headers["authorization"]
+    const token = authHeader && authHeader.split(' ')[1] // because we only want the token portion of the authHeader = "Bearer <token>" 
+    if (token == null) return res.status(401).json({message: "No token provided"})
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.status(401).json({message: "Token not authorized"});
+        req.user = user
+        console.log(user)
+        next()
+    })
+}
+
+export {verifyToken, verifyTokenTest}
