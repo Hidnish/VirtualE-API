4ddfb24d1496de66eefa18026a581cf3cf87524f
Hidnish <hideonishimura@live.it>
2022-09-22 10:09:44 +0200

    Add login endpoint to routers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import {Router} from 'express'
-import { getUsers, addUser, getUser, deleteUser } from '../controllers/user.controller.js';
+import { getUsers, addUser, getUser, deleteUser, login } from '../controllers/user.controller.js';
 const router = Router();
 
 router.get('/getUsers', getUsers);
 router.get('/getUser/:userId', getUser);
 router.post('/addUser', addUser);
+router.post('/login', login);
 router.delete('/deleteUser/:userId', deleteUser);
 
 export default router;
