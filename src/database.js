6cccf1d4ceaaeffe6dbc56fe149952a97d31826a
Omar Alsyouf <omaralsoyouf@gmail.com>
2022-10-15 11:16:42 +0100

    allow multipleStatments

--- src/database.js
+++ src/database.js
@@ -1,16 +1,17 @@
 import mysql from 'promise-mysql';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 dotenv.config();
 
 const connection = mysql.createConnection({
-	host: process.env.SQL_HOST,
-	database: process.env.SQL_DATABASE,
-	user: process.env.SQL_USER,
-	password: process.env.SQL_PASSWORD,
-})
+  host: process.env.SQL_HOST,
+  database: process.env.SQL_DATABASE,
+  user: process.env.SQL_USER,
+  password: process.env.SQL_PASSWORD,
+  multipleStatements: true,
+});
 
 const getConnection = () => {
-	return connection
-}
+  return connection;
+};
 
-export { getConnection }
+export { getConnection };
